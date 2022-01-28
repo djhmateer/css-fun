27c9f2f3c45c975b7e1791eaf6ec68b8e740a737
Dave Mateer <davemateer@gmail.com>
2022-01-28 07:36:34 +0000

    auto

--- 61osr-project-starbucks/script.js
+++ 61osr-project-starbucks/script.js
@@ -1,13 +1,12 @@
 
-// const btn = document.getElementById('menu-btn');
-// const nav = document.getElementById('menu');
+const btn = document.getElementById('menu-btn');
+const mobmenu = document.getElementById('mob-menu');
 
-// function navToggle() {
-// 	// adds / removes open class from btn element
-// 	btn.classList.toggle('open');
-// 	nav.classList.toggle('hidden');
-// 	document.body.classList.toggle('no-scroll');
+function navToggle() {
+	// adds / removes open class from btn element
+	btn.classList.toggle('open');
+	mobmenu.classList.toggle('hidden');
+	document.body.classList.toggle('no-scroll');
+}
 
-// }
-
-// btn.addEventListener('click', navToggle)
+btn.addEventListener('click', navToggle)
